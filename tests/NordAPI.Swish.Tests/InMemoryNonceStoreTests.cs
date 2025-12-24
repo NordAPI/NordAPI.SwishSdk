564849c993b10d244f080a072ba2aec27fe1568b
NordAPI-admin <info@nordapi.com>
2025-12-24 11:39:54 +0100

    fix: make InMemoryNonceStore nonce tracking atomic (#177)

---
+++ tests/NordAPI.Swish.Tests/InMemoryNonceStoreTests.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using NordAPI.Swish.Webhooks;
+using Xunit;
+
+namespace NordAPI.Swish.Tests;
+
+public sealed class InMemoryNonceStoreTests
+{
+    [Fact]
+    public async Task TryRememberAsync_IsAtomic_UnderConcurrency()
+    {
+        using var store = new InMemoryNonceStore(TimeSpan.FromMinutes(10));
+
+        var nonce = "same-nonce";
+        var expiresAtUtc = DateTimeOffset.UtcNow.AddMinutes(5);
+
+        var start = new ManualResetEventSlim(false);
+
+        var tasks = Enumerable.Range(0, 100)
+            .Select(_ => Task.Run(async () =>
+            {
+                start.Wait();
+                return await store.TryRememberAsync(nonce, expiresAtUtc);
+            }))
+            .ToArray();
+
+        start.Set();
+
+        var results = await Task.WhenAll(tasks);
+
+        Assert.Equal(1, results.Count(r => r));
+        Assert.Equal(results.Length - 1, results.Count(r => !r));
+    }
+}
